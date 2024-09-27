7d664bd60acefdd7a6100eb165b96a837190b606
JuanMartin3 <145402763+JuanMartin3@users.noreply.github.com>
2024-09-27 18:38:24 -0400

    UI del menu principal

---
+++ Proyecto1SO/Proyecto1SO/src/Clases/Trabajador.java
@@ -0,0 +1,92 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Clases;
+
+/**
+ *
+ * @author Windows 10
+ */
+public class Trabajador {
+    protected String nombre;
+    protected int cedula;
+    protected double salarioPorHora;
+    protected boolean estaTrabajando;
+    protected int horasTrabajadas;
+    protected Almacen almacenAsignado;
+    
+    //constructor 
+    public Trabajador(String nombre, int cedula, double salarioPorHora, Almacen almacenAsignado, int horasTrabajadas){
+        this.nombre = nombre;
+        this.cedula = cedula;
+        this.salarioPorHora = salarioPorHora;
+        this.estaTrabajando = false; //empieza sin estar trabajando
+        this.horasTrabajadas = 0; //empieza con 0 horas
+        this.almacenAsignado = almacenAsignado;
+    }
+    
+    //Metodos comunes
+    public void emprezarTurno(){
+        this.estaTrabajando = true;
+    }
+    
+    public void terminarTurno(){
+        this.estaTrabajando = false;
+        this.horasTrabajadas += 24;
+    }
+    
+    public double calcularSalario(){
+        return this.horasTrabajadas * this.salarioPorHora;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public int getCedula() {
+        return cedula;
+    }
+
+    public double getSalarioPorHora() {
+        return salarioPorHora;
+    }
+
+    public boolean isEstaTrabajando() {
+        return estaTrabajando;
+    }
+
+    public int getHorasTrabajadas() {
+        return horasTrabajadas;
+    }
+
+    public Almacen getAlmacenAsignado() {
+        return almacenAsignado;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setCedula(int cedula) {
+        this.cedula = cedula;
+    }
+
+    public void setSalarioPorHora(double salarioPorHora) {
+        this.salarioPorHora = salarioPorHora;
+    }
+
+    public void setEstaTrabajando(boolean estaTrabajando) {
+        this.estaTrabajando = estaTrabajando;
+    }
+
+    public void setHorasTrabajadas(int horasTrabajadas) {
+        this.horasTrabajadas = horasTrabajadas;
+    }
+
+    public void setAlmacenAsignado(Almacen almacenAsignado) {
+        this.almacenAsignado = almacenAsignado;
+    }
+    
+    
+}
---
+++ Proyecto1SO/Proyecto1SO/src/Clases/tipoComponente.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Clases;
+
+/**
+ *
+ * @author Windows 10
+ */
+public enum tipoComponente {
+    PLACA_BASE,
+    CPU,
+    RAM,
+    FUENTE_ALIMENTACION,
+    TARJETA_GRAFICA
+}
